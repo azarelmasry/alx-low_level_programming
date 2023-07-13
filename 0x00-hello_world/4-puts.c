904e324ffeaec974950bd0caac9b960aea34672b
azarelmasry <azarelmasry@gmail.com>
2023-07-13 13:59:35 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A  program that a link  print with puts function 
+ * main - A program that a line prints with puts function 
  * Return: 0 (Success)
  */
 int main(void)
