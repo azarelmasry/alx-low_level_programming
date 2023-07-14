9bd8c9c790f6e59b01728c14721590a3c3c4993f
azarelmasry <azarelmasry@gmail.com>
2023-07-14 16:33:32 -0700

    hello world

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+putchar (ch);
+ch++;
+putchar('\n');
+return (0);
+}
