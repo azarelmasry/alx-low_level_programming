0d487c9fce84b52c0edbeedeb07ff79ad0d3b660
azarelmasry <azarelmasry@gmail.com>
2023-07-14 22:06:54 -0700

    hello world

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int digit = 48; /*48; decimal rep of 0*/
+while (digit <= 102) /*102; decimal rep of f*/
+{
+putchar (digit);
+/* after 9 we jump till 96; */
+if (digit == 57)
+digit += 39;
+digit++;
+}
+putchar('\n');
+return (0);
+}
