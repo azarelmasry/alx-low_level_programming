31d26fb9f9b5a0a0109fd605b5b08a9916fc85d0
azarelmasry <azarelmasry@gmail.com>
2023-07-15 01:55:07 -0700

    hello world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int firstDigit = 0, seconDigit;
+while (firstDigit <= 99)
+{
+seconDigit = firstDigit;
+while (seconDigit <= 99)
+{
+if (seconDigit != firstDigit)
+{
+putchar((firstDigit / 10) + 48);
+putchar((firstDigit % 10) + 48);
+putchar(' ');
+putchar((seconDigit / 10) + 48);
+putchar((seconDigit % 10) + 48);
+if (firstDigit != 98 || seconDigit != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+seconDigit++;
+}
+firstDigit++;
+}
+putchar('\n');
+return (0);
+}
