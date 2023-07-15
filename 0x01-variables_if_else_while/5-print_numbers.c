9ea62ed49900ff2aca2996123aa5bcc6ae91c773
azarelmasry <azarelmasry@gmail.com>
2023-07-14 21:28:51 -0700

    hello world

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+printf("%i", digit);
+digit++;
+}
+printf("\n");
+return (0);
+}
