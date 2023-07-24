63e050e696ffc946c50f0dec0f4aa2fc71f440e5
azarelmasry <azarelmasry@gmail.com>
2023-07-24 08:34:38 -0700

    arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,7 +14,7 @@ for (l = 0; s[l] != '\0'; ++l)
 /*swap the string by looping to half the string*/
 for (i = 0; i < l / 2; i++)
 {
-temp s[i];
+temp = s[i];
 s[i] = s[l - 1 - i]; /* -1 because the array starts from 0 */
 s[l - 1 - i] = temp;
 }
