4b153b3f316fd730074eb6e443b1ef011f07098e
azarelmasry <azarelmasry@gmail.com>
2023-08-08 06:35:09 -0700

    free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,18 +19,18 @@ return (size);
 */
 char *str_concat(char *s1, char *s2)
 {
-int sizel, size2, i;
+int size1, size2, i;
 char *m;
 if (s1 == NULL)
 s1 = "\0";
 if (s2 == NULL)
 s2 = "\0";
-sizel = _strlen(s1);
+size1 = _strlen(s1);
 size2 = _strlen(s2);
 m = malloc((sizel + size2) *sizeof(char) + 1);
 if (m == 0)
 return (0);
-for (i = 0; i <= sizel + size2; i++)
+for (i = 0; i <= size1 + size2; i++)
 {
 if (i < size1)
 m[i] = s1[i];
