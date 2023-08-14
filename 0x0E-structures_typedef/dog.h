7b6011dec2bc1489b7344613e988fc0903b377fb
azarelmasry <azarelmasry@gmail.com>
2023-08-14 06:04:46 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,7 @@ char *name;
 float age;
 char *owner;
 };
-typedef struct gog dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
