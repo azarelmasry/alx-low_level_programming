218c4b21a109d13d20e7b251c0e36762506e879f
azarelmasry <azarelmasry@gmail.com>
2023-10-12 06:53:11 -0700

    17pc

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+* dlistint_len - returns length
+* @h: address of head node
+* Return: size of list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t i = 0;
+while (h)
+{
+h = h->next;
+i++;
+}
+return (i);
+}
